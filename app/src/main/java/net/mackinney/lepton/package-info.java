7a41f5f7d54ce565172ad6c31cb197f9505cdf7c
pmackinney <paul@mackinney.net>
2021-01-29 20:42:20 -0800

    git rm administrative

--- app/src/main/java/net/mackinney/lepton/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * A backgammon client compatible with the First Internet Backgammon Server (FIBS)
- * designed to run on an Android device and developed with Android Studio.
- */
-package net.mackinney.lepton;
